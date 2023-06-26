35ca0334a6ec6e22a64c0a56b82c7ce616db8d04
Alex <halos959@gmail.com>
2023-06-25 23:09:06 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
+// 404 Page
+app.get('*', function (req, res) {
+  res.send('404')
+})
+
   // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-  
   // Breads
   const breadsController = require('./controllers/breads_controller')
   app.use('/breads', breadsController)
